b88c23f23842e9f42ceff55016ce6a463d67e0c7
Marshall <you111@example.com>
2023-04-20 02:22:55 -0500

    v5.0

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import test_model
+from rest_framework import viewsets
+from rest_framework import permissions
+#from rest_framework import authentication
+from .serializer import test_serializer
 
-# Create your views here.
+class test_viewset(viewsets.ModelViewSet):
+    queryset = test_model.objects.all()
+    serializer_class = test_serializer
+    #authentication_classes = [authentication.SessionAuthentication] #test
+    permission_classes = [permissions.IsAuthenticated]
